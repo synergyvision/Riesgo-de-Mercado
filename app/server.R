40330a89037a498f3e5a6b4d8f0b08c6e9666b6e
Freddy Tapia <Freddy Tapia>
2018-08-09 16:30:08 -0400

    añado metodología Nelson y Siegel

--- app/server.R
+++ app/server.R
@@ -1,36 +1,60 @@
 shinyServer(function(input, output) {
 
   #fechas
+  #Svensson
   output$p1<-renderPrint({paste(substr(input$n1,9,10),substr(input$n1,6,7),substr(input$n1,1,4),sep = "/")})
+  #Nelson y Siegel
   output$p2<-renderPrint({paste(substr(input$n2,9,10),substr(input$n2,6,7),substr(input$n2,1,4),sep = "/")})
+  #Diebold-Li
   output$p3<-renderPrint({paste(substr(input$n3,9,10),substr(input$n3,6,7),substr(input$n3,1,4),sep = "/")})
+  #Splines
+  output$p4<-renderPrint({paste(substr(input$n4,9,10),substr(input$n4,6,7),substr(input$n4,1,4),sep = "/")})
   #titulos
   #tif
   output$q1<-renderPrint({c(input$t1,input$t2,input$t3,input$t4)})
+  output$q1_ns<-renderPrint({c(input$t1_ns,input$t2_ns,input$t3_ns,input$t4_ns)})
   #vebonos
   output$q2<-renderPrint({c(input$v1,input$v2,input$v3,input$v4)})
+  output$q2_ns<-renderPrint({c(input$v1_ns,input$v2_ns,input$v3_ns,input$v4_ns)})
   #precios
   tf <- reactive({pos(c(input$t1,input$t2,input$t3,input$t4),0)})
+  tf_ns <- reactive({pos(c(input$t1_ns,input$t2_ns,input$t3_ns,input$t4_ns),0)})
   tv <- reactive({pos(c(input$v1,input$v2,input$v3,input$v4),1)})
+  tv_ns <- reactive({pos(c(input$v1_ns,input$v2_ns,input$v3_ns,input$v4_ns),1)})
   output$pre1 <-renderPrint({tf()})
+  output$pre1_ns <-renderPrint({tf_ns()})
   output$pre2 <-renderPrint({tv()})
+  output$pre2_ns <-renderPrint({tv_ns()})
   
   #parametros
   output$pa_tif <- renderPrint({pa})
+  output$pa_tif_ns <- renderPrint({pa_ns})
   output$pa_veb <- renderPrint({pa1})
+  output$pa_veb_ns <- renderPrint({pa1_ns})
   
   #muestro caracteristicas
   output$Ca <- renderDataTable({head(C)})
+  output$Ca_ns <- renderDataTable({head(C)})
   output$Ca1 <- renderDataTable({head(C)})
+  output$Ca1_ns <- renderDataTable({head(C)})
   
   #precios estimados iniciales
   output$p_est_tif <- renderDataTable({Tabla.sven(fv = input$n1 ,tit = c(input$t1,input$t2,input$t3,input$t4),pr =tf() ,pa = pa,ind = 0,C = C,fe2=0,fe3=0) })
+  output$p_est_tif_ns <- renderDataTable({Tabla.ns(fv = input$n2 ,tit = c(input$t1_ns,input$t2_ns,input$t3_ns,input$t4_ns),pr =tf_ns() ,pa = pa_ns,ind = 0,C = C,fe2=0,fe3=0) })
+  
+  
   output$p_est_veb <- renderDataTable({Tabla.sven(fv = input$n1 ,tit = c(input$v1,input$v2,input$v3,input$v4),pr =tv() ,pa = pa1,ind = 1,C = C,fe2=0,fe3=0) })
+  output$p_est_veb_ns <- renderDataTable({Tabla.ns(fv = input$n2 ,tit = c(input$v1_ns,input$v2_ns,input$v3_ns,input$v4_ns),pr =tv_ns() ,pa = pa1_ns,ind = 1,C = C,fe2=0,fe3=0) })
+  
   
   #precios estimados optimizados
   #paquete alabama
   output$p_est_tif_opt <- renderDataTable({Tabla.sven(fv = input$n1 ,tit = c(input$t1,input$t2,input$t3,input$t4),pr =tf() ,pa = pa,ind = 0,C = C,fe2=input$opt_tif,fe3=0) })
+  output$p_est_tif_opt_ns <- renderDataTable({Tabla.ns(fv = input$n2 ,tit = c(input$t1_ns,input$t2_ns,input$t3_ns,input$t4_ns),pr =tf_ns() ,pa = pa_ns,ind = 0,C = C,fe2=input$opt_tif_ns,fe3=0) })
+  
+  
   output$p_est_veb_opt <- renderDataTable({Tabla.sven(fv = input$n1 ,tit = c(input$v1,input$v2,input$v3,input$v4),pr =tv() ,pa = pa1,ind = 1,C = C,fe2=input$opt_veb,fe3=0) })
+  output$p_est_veb_opt_ns <- renderDataTable({Tabla.ns(fv = input$n2 ,tit = c(input$v1_ns,input$v2_ns,input$v3_ns,input$v4_ns),pr =tv_ns() ,pa = pa1_ns,ind = 1,C = C,fe2=input$opt_veb_ns,fe3=0) })
   
   
   # Almacenar Variables Reactivas
--- app/ui.R
+++ app/ui.R
@@ -33,7 +33,9 @@ shinyUI(
           
           menuSubItem("Metodología Nelson y Siegel", tabName = "subitem2", icon = icon("circle-o")),
           
-          menuSubItem("Metodología Splines", tabName = "subitem3", icon = icon("circle-o"))
+          menuSubItem("Metodología Diebold-Li", tabName = "subitem3", icon = icon("circle-o")),
+          
+          menuSubItem("Metodología Splines", tabName = "subitem4", icon = icon("circle-o"))
         )
         ,
         menuItem("Acerca", icon = icon("exclamation-circle"), tabName = "acerca"))
@@ -98,12 +100,66 @@ shinyUI(
                    fluidRow( column( width = 6,box( width = 12, background = "navy",
                                                     dateInput(inputId="n2", label="Por favor, seleccionar una fecha", language= "es",
                                                               width = "100%") )),box( width = 6,height = 2,title = "Fecha de valoración: ",verbatimTextOutput('p2')) )
-        ), tabItem(tabName = "subitem3",
+        
+              ,
+              h2("  Títulos"), h5("  Favor seleccionar los títulos a considerar: "),
+              fluidRow(tabBox( width = 12, title = "Títulos", id = "tab1", height = "50px", 
+                               tabPanel("TIF",
+                                        fluidRow(column(width = 3,checkboxGroupInput( inputId = "t1_ns", label = " ",
+                                                                                      choices=tit[1:7])),
+                                                 column(width = 3,checkboxGroupInput( inputId = "t2_ns", label = " ",
+                                                                                      choices=tit[8:13])), column(width = 3,checkboxGroupInput( inputId = "t3_ns", 
+                                                                                                                                                label = " ",choices=tit[14:19])),column(width = 3,
+                                                                                                                                                                                        checkboxGroupInput( inputId = "t4_ns", label = " ",choices=tit[20:25])) ) ,
+                                        
+                                        verbatimTextOutput("q1_ns"),h2(" Precios Promedios"),verbatimTextOutput("pre1_ns"),
+                                        h2(" Parámetros iniciales"),verbatimTextOutput("pa_tif_ns"),
+                                        h2(" Características"),dataTableOutput("Ca_ns"),
+                                        h2(" Precios estimados iniciales"),dataTableOutput("p_est_tif_ns"),
+                                        h2(" Precios estimados optimizados"),
+                                        radioButtons( inputId = "opt_tif_ns",label = "Desea optimizar los precios obtenidos:", 
+                                                      choices = c("Si"=1, "No"=0),
+                                                      selected=" ")
+                                        ,dataTableOutput("p_est_tif_opt_ns") 
+                                        ),
+                               tabPanel("VEBONO",fluidRow(column(width = 3,
+                                                                 checkboxGroupInput( inputId = "v1_ns", label = " ",
+                                                                                     choices=tit1[1:8])),
+                                                          column(width = 3,checkboxGroupInput( inputId = "v2_ns", label = " ",
+                                                                                               choices=tit1[9:16])), column(width = 3,checkboxGroupInput( inputId = "v3_ns", 
+                                                                                                                                                          label = " ",choices=tit1[17:24])),column(width = 3,
+                                                                                                                                                                                                   checkboxGroupInput( inputId = "v4_ns", label = " ",choices=tit1[25:29])) ) ,
+                                        verbatimTextOutput("q2_ns"), h2(" Precios Promedios"),verbatimTextOutput("pre2_ns"),
+                                         h2(" Parámetros iniciales"),verbatimTextOutput("pa_veb_ns") ,
+                                         h2(" Características"),dataTableOutput("Ca1_ns"),
+                                         h2(" Precios estimados iniciales"),dataTableOutput("p_est_veb_ns"),
+                                         h2(" Precios estimados optimizados"),
+                                        radioButtons( inputId = "opt_veb_ns",label = "Desea optimizar los precios obtenidos:", 
+                                                      choices = c("Si"=1, "No"=0),
+                                                      selected=" "),dataTableOutput("p_est_veb_opt_ns")
+                                        )
+                               
+                               
+              )
+              
+              )
+              
+              ), 
+      tabItem(tabName = "subitem3",
+              
+              h2("Diebold-Li"),
+              fluidRow( column( width = 6,box( width = 12, background = "navy",
+                                               dateInput(inputId="n3", label="Por favor, seleccionar una fecha", language= "es",
+                                                         width = "100%") )),box( width = 6,height = 2,title = "Fecha de valoración: ",verbatimTextOutput('p3')))
+      )
+      
+      
+      ,tabItem(tabName = "subitem4",
                    
                    h2("Splines"),
                    fluidRow( column( width = 6,box( width = 12, background = "navy",
-                                                    dateInput(inputId="n3", label="Por favor, seleccionar una fecha", language= "es",
-                                                              width = "100%") )),box( width = 6,height = 2,title = "Fecha de valoración: ",verbatimTextOutput('p3')))
+                                                    dateInput(inputId="n4", label="Por favor, seleccionar una fecha", language= "es",
+                                                              width = "100%") )),box( width = 6,height = 2,title = "Fecha de valoración: ",verbatimTextOutput('p4')))
         ),
         tabItem(tabName = "acerca",
                 box( width = 9, status="warning",
