2d1b12eea46b5197a423fa5f867b52c528508f12
Freddy Tapia <freddytapia@Bachelier.local>
2018-12-13 12:37:08 -0400

    agrego cambios script montecarlo_apoyo

--- app/Scripts auxiliares/montecarlo_apoyo.R
+++ app/Scripts auxiliares/montecarlo_apoyo.R
@@ -62,7 +62,138 @@ f <- function(x)  exp(-0.5*x^2)/sqrt(2*pi)
   
   pnorm(1)-pnorm(0)
   
+####################################
+####################################
+#calculo VaR por simulacion de Montecarlo 1 activo
+
+#leo data
+  #Prueba VaR, usando paquete VaRES
+  library(corpcor)
+  library(tseries)
+  library(fitdistrplus)
+  library(rriskDistributions)
+  #quito notacion cientifica
+  options(scipen=999) 
   
+  #cargo la data
+  tif <- read.delim2("~/Downloads/tif.txt")
+  veb <- read.delim2("~/Downloads/vebono.txt")
+  data_montecarlo <- read.delim2("~/Downloads/Data-Montecarlo-Diario.txt")
+  
+  #ordeno la data de fecha mas reciente a fecha mas antigua
+  tif$Fecha <- as.Date(tif$Fecha)
+  veb$Fecha <- as.Date(veb$Fecha)
+  data_montecarlo$Fecha <- as.Date(data_montecarlo$Fecha)
+  
+  #quito tif vencidos
+  tif <- tif[order(tif$Fecha,decreasing = TRUE),-c(2,3)]
+  veb <- veb[order(veb$Fecha,decreasing = TRUE),]
+  data_montecarlo <- data_montecarlo[order(data_montecarlo$Fecha,decreasing = TRUE),]
+  
+  #pruebo con TIF
+  #seleciono 252 obs
+  tif_252 <- tif[1:252,]
+
+  #valor nominal TIF
+  #tif
+  a <- as.data.frame(matrix(0,nrow = (ncol(tif)-1),ncol=2))
+  names(a) <- c("titulo","valor_nominal")
+  a[,1] <- names(tif)[-1]
+  
+  vt <- c(2000000,10000000,5000000,7000000,1000000,
+          2000000,4000000,3000000,20000000,6000000,8000000,2000000,
+          10000000,30000000,6000000,10000000,5000000,9000000,5000000,
+          5000000,9000000,4000000,70000000,5000000,7000000)
+  
+  a[,2] <- vt
+
+  #1) leer datos de precio (ver arriba) 
+  #la variable con la que se trabajara es tif_252
+  head(tif_252)
+  
+  #2)calcular rend a los precios 
+  rend_tif <- as.data.frame(matrix(0,nrow = (nrow(tif_252)-1),ncol = (ncol(tif_252)-1)))
+  names(rend_tif) <- names(tif_252)[-1]
+  
+  
+  for(i in 1:(ncol(tif_252)-1)){
+    rend_tif[,i] <- diff(log(tif_252[,(i+1)]))
+  }
+
+  
+  #trabajo con el primer instrumeto, la columna 1!
+  d <- useFitdist(rend_tif[,1]) 
+  
+  #asumo normalidad
+  #genero numero aleatorios segun dist
+  n_norm <- rnorm(n = 100000,mean = as.numeric(fitdistr(rend_tif[,1],"normal")$estimate)[1],sd = as.numeric(fitdistr(rend_tif[,1],"normal")$estimate)[2])
+  
+  #calculo incrementos 
+  pre_inc_n <- vt[1]*n_norm
+  
+  #precio
+  pre_n <- vt[1]-pre_inc_n
+  
+  #calculo ubicacion
+  length(n_norm)*5/100
+  
+  #valor corte
+  #ordeno precios
+  pre1_n <- pre_n[order(pre_n)]
+  hist(pre1_n)
+  vc_n <- pre1_n[length(n_norm)*5/100]
+  
+  #VaR
+  var_n <- vt[1]-vc_n
+  
+  #asumo dist logistica
+  #genero numero aleatorios segun dist
+  n_log <- rlogis(n = 100000,location =  as.numeric(d$fit.list$Logistic$estimate)[1],scale =  as.numeric(d$fit.list$Logistic$estimate)[2])
+  
+  #calculo incrementos 
+  pre_inc_log <- vt[1]*n_log
+  
+  #precio
+  pre_log <- vt[1]-pre_inc_log
+  
+  #calculo ubicacion
+  length(n_log)*5/100
+  
+  #valor corte
+  #ordeno precios
+  pre1_log <- pre_log[order(pre_log)]
+  hist(pre1_log)
+  vc_log <- pre1_log[length(n_log)*5/100]
+  
+  #VaR
+  var_log <- vt[1]-vc_log
+  
+  
+  #asumo dist uniforme
+  #genero numero aleatorios segun dist
+  n_u <- runif(n = 100000,min  =  as.numeric(d$fit.list$Uniform$estimate)[1],max =  as.numeric(d$fit.list$Uniform$estimate)[2])
+  
+  #calculo incrementos 
+  pre_inc_u <- vt[1]*n_u
+  
+  #precio
+  pre_u <- vt[1]-pre_inc_u
+  
+  #calculo ubicacion
+  length(n_u)*5/100
+  
+  #valor corte
+  #ordeno precios
+  pre1_u <- pre_u[order(pre_u)]
+  hist(pre1_u)
+  vc_u <- pre1_u[length(n_u)*5/100]
+  
+  #VaR
+  var_u <- vt[1]-vc_u
+  
+  #comparacion normal vs logistica vs uniforme
+  c(var_n,var_log,var_u)
+    
   
   
     
