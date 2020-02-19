f59053fb9a65c3cff8380d274f29a14dbc586635
Fr3ddy1 <ft@synergy.vision>
2020-02-19 17:12:46 -0400

    Agrego historico de precios por mes Svensson

--- app/Scripts auxiliares/Precios_diarios.R
+++ app/Scripts auxiliares/Precios_diarios.R
@@ -1274,18 +1274,347 @@ write.table(q1,"precios_sven_tif_may_19.txt")
 
 
 #JUNIO 2019
+jun <- c("03/06/2019","04/06/2019","05/06/2019","06/06/2019","07/06/2019",
+         "10/06/2019","11/06/2019","12/06/2019","13/06/2019","14/06/2019",
+         "17/06/2019","18/06/2019","19/06/2019","20/06/2019","21/06/2019",
+         "24/06/2019","25/06/2019","26/06/2019","27/06/2019","28/06/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-05-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(jun)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=jun[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+  
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- jun
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_jun_19.txt")
+
 
 #JULIO 2019
+jul <- c("01/07/2019","02/07/2019","03/07/2019","04/07/2019","05/07/2019",
+         "08/07/2019","09/07/2019","10/07/2019","11/07/2019","12/07/2019",
+         "15/07/2019","16/07/2019","17/07/2019","18/07/2019","19/07/2019",
+         "22/07/2019","23/07/2019","24/07/2019","25/07/2019","26/07/2019",
+         "29/07/2019","30/07/2019","31/07/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-05-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#PRECIOS SVENSSON
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(jul)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=jul[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- jul
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_jul_19.txt")
+
 
 #AGOSTO 2019
+ago <- c("01/08/2019","02/08/2019","05/08/2019","06/08/2019","07/08/2019",
+         "08/08/2019","09/08/2019","12/08/2019","13/08/2019","14/08/2019",
+         "15/08/2019","16/08/2019","19/08/2019","20/08/2019","21/08/2019",
+         "22/08/2019","23/08/2019","26/08/2019","27/08/2019","28/08/2019",
+         "29/08/2019","30/08/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-08-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(ago)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=ago[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- ago
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_ago_19.txt")
+
 
 #SEPTIEMBRE 2019
+sep <- c("02/09/2019","03/09/2019","04/09/2019","05/09/2019","06/09/2019",
+         "09/09/2019","10/09/2019","11/09/2019","12/09/2019","13/09/2019",
+         "16/09/2019","17/09/2019","18/09/2019","19/09/2019","20/09/2019",
+         "23/09/2019","24/09/2019","25/09/2019","26/09/2019","27/09/2019",
+         "30/09/2019")
+
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-09-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(sep)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=sep[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- sep
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_sep_19.txt")
+
 
 #OCTUBRE 2019
+oct <- c("01/10/2019","02/10/2019","03/10/2019","04/10/2019","07/10/2019",
+         "08/10/2019","09/10/2019","10/10/2019","11/10/2019","14/10/2019",
+         "15/10/2019","16/10/2019","17/10/2019","18/10/2019","21/10/2019",
+         "22/10/2019","23/10/2019","24/10/2019","25/10/2019","28/10/2019",
+         "29/10/2019","30/10/2019","31/10/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/11-10-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(oct)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=oct[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- oct
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_oct_19.txt")
+
 
 #NOVIEMBRE 2019
+nov <- c("01/11/2019","04/11/2019","05/11/2019","06/11/2019","07/11/2019",
+         "08/11/2019","11/11/2019","12/11/2019","13/11/2019","14/11/2019",
+         "15/11/2019","18/11/2019","19/11/2019","20/11/2019","21/11/2019",
+         "22/11/2019","25/11/2019","26/11/2019","27/11/2019","28/11/2019",
+         "29/11/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/07-11-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(nov)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=nov[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- nov[1:12]
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_nov_19a.txt")
+
+#CALCULO NUEVOS PRECIOS SIN TITULO Q VENCIO
+tit <- tit[-26]
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 14:length(nov)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=nov[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- nov[14:21]
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+
+
+write.table(q1,"precios_sven_tif_nov_19b.txt")
+
 
 #DICIEMBRE 2019
+dic <- c("02/12/2019","03/12/2019","04/12/2019","05/12/2019","06/12/2019",
+         "09/12/2019","10/12/2019","11/12/2019","12/12/2019","13/12/2019",
+         "16/12/2019","17/12/2019","18/12/2019","19/12/2019","20/12/2019",
+         "23/12/2019","24/12/2019","25/12/2019","26/12/2019","27/12/2019",
+         "30/12/2019","31/12/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-12-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="TIF"])))
+
+#busco precio promedio
+pp <- pos1(tit,0,Precio_prom_tif)
+
+pp[which(pp==0)]
+
+
+#
+
+p2 <- rep(0,length(tit[-which(pp==0)])+1)
+
+for(i in 1:length(dic)){
+  print(i)
+  p1 <- try(Tabla.sven(fv=dic[i],tit[-which(pp==0)],pos1(tit[-which(pp==0)],0,Precio_prom_tif),pa=c(1,1,1,1,1,1),ind=0,C=ca,fe2=1,fe3=0)[[3]])
+  
+  if(!is.null(p1)){
+    p2 <- cbind.data.frame(p2,p1$Precios)  
+  }else{
+    p1 <- rep(0,length(tit[-which(pp==0)])+1)
+    p2 <- cbind.data.frame(p2,p1) 
+  }
+}
+
+q <- p2[-nrow(p2),-1]
+names(q) <- dic
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_sven_tif_dic_19.txt")
+
 
 
 ##############
