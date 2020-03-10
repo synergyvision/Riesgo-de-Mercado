e7c0b77f7cd564864cf9c7bf79bd86cdb360ecbf
Fr3ddy1 <ft@synergy.vision>
2020-03-10 16:14:47 -0400

    Agrego historico NS VEB 2019

--- app/Scripts auxiliares/Apoyo_historico_VEB.R
+++ app/Scripts auxiliares/Apoyo_historico_VEB.R
@@ -323,6 +323,150 @@ q1 <- t(q)
 
 write.table(q1,"precios_ns_veb_ago_19.txt")
 
+#SEPTIEMBRE 2019
+sep <- c("02/09/2019","03/09/2019","04/09/2019","05/09/2019","06/09/2019",
+         "09/09/2019","10/09/2019","11/09/2019","12/09/2019","13/09/2019",
+         "16/09/2019","17/09/2019","18/09/2019","19/09/2019","20/09/2019",
+         "23/09/2019","24/09/2019","25/09/2019","26/09/2019","27/09/2019",
+         "30/09/2019")
+
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/30-09-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="VEBONO"])))
+
+#busco precio promedio
+pp <- pos1(tit,1,Precio_prom_veb)
+
+q1 <- pre_veb(sep,ca)
+
+p2 <- q1
+
+q <- p2[-nrow(p2),-1]
+names(q) <- sep
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_ns_veb_sep_19.txt")
+
+
+#OCTUBRE 2019
+oct <- c("01/10/2019","02/10/2019","03/10/2019","04/10/2019","07/10/2019",
+         "08/10/2019","09/10/2019","10/10/2019","11/10/2019","14/10/2019",
+         "15/10/2019","16/10/2019","17/10/2019","18/10/2019","21/10/2019",
+         "22/10/2019","23/10/2019","24/10/2019","25/10/2019","28/10/2019",
+         "29/10/2019","30/10/2019","31/10/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/11-10-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="VEBONO"])))
+
+#busco precio promedio
+pp <- pos1(tit,1,Precio_prom_veb)
+
+q1 <- pre_veb(oct,ca)
+
+p2 <- q1
+
+q <- p2[-nrow(p2),-1]
+names(q) <- oct
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_ns_veb_oct_19.txt")
+
+#NOVIEMBRE 2019
+nov <- c("01/11/2019","04/11/2019","05/11/2019","06/11/2019","07/11/2019",
+         "08/11/2019","11/11/2019","12/11/2019","13/11/2019","14/11/2019",
+         "15/11/2019","18/11/2019","19/11/2019","20/11/2019","21/11/2019",
+         "22/11/2019","25/11/2019","26/11/2019","27/11/2019","28/11/2019",
+         "29/11/2019")
+
+#leo caracteristicas
+ca <- Carac("C:/Users/Ecuad/Downloads/07-11-2019.xls")
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="VEBONO"])))
+
+#busco precio promedio
+pp <- pos1(tit,1,Precio_prom_veb)
+
+q1 <- pre_veb(nov,ca)
+
+p2 <- q1
+
+q <- p2[-nrow(p2),-1]
+names(q) <- nov
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_ns_veb_nov_19.txt")
+
+#DICIEMBRE 2019
+dic <- c("02/12/2019","03/12/2019","04/12/2019","05/12/2019","06/12/2019",
+         "09/12/2019","10/12/2019","11/12/2019","12/12/2019","13/12/2019",
+         "16/12/2019","17/12/2019","18/12/2019","19/12/2019","20/12/2019",
+         "23/12/2019","24/12/2019","25/12/2019","26/12/2019","27/12/2019",
+         "30/12/2019","31/12/2019")
+
+#leo caracteristicas
+#ca <- Carac("C:/Users/Ecuad/Downloads/30-12-2019.xls")
+ca <- Carac("C:/Users/Ecuad/Downloads/07-11-2019.xls")
+
+names(ca) <- c("Tipo Instrumento","Nombre","Sicet","F.Emision",
+               "F.Vencimiento","Tipo tasa","Inicio","Pago cupon 1" ,
+               "Pago cupon 2","Cupon")
+
+tit <- levels(as.factor(as.character(ca$Nombre[ca$`Tipo Instrumento`=="VEBONO"])))
+
+#busco precio promedio
+pp <- pos1(tit,1,Precio_prom_veb)
+
+q1 <- pre_veb(dic[1:21],ca)
+
+p2 <- q1
+
+q <- p2[-nrow(p2),-1]
+names(q) <- dic[1:21]
+row.names(q) <- tit[-which(pp==0)]
+q1 <- t(q)
+
+write.table(q1,"precios_ns_veb_dica_19.txt")
+
+#JUNTO TODA LA DATA NS 2019
+#VEBONO
+ene_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_ene_19.txt", sep="")
+feb_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_feb_19.txt", sep="")
+mar_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_mar_19.txt", sep="")
+abr_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_abr_19.txt", sep="")
+may_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_may_19.txt", sep="")
+jun_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_jun_19.txt", sep="")
+jul_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_jul_19.txt", sep="")
+ago_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_ago_19.txt", sep="")
+sep_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_sep_19.txt", sep="")
+oct_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_oct_19.txt", sep="")
+nov_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_nov_19.txt", sep="")
+dic_19 <- read.csv("~/Riesgo-de-Mercado/precios_ns_veb_dica_19.txt", sep="")
+
+NS_19 <- rbind.data.frame(ene_19,feb_19,mar_19,abr_19,may_19,jun_19,
+                          jul_19,ago_19,sep_19,oct_19,nov_19,dic_19)
+
+#
+write.table(NS_19,"HIST_VEB_NS_19.txt")
+
+
+
+
 ##############
 ##############
 ##############
